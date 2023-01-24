d7b230e6924fec2cdf72d36df3da43dee461bfca
Jaremy Creechley <creechley@gmail.com>
2023-01-24 14:57:05 -0700

    testing ifdef

--- tests/ctests/basic.c
+++ tests/ctests/basic.c
@@ -1,6 +1,14 @@
+#ifdef __unix__
+const int unix = 1;
+#else
+const int unix = 0;
 #include <stdio.h>
+#endif
+#include <stdio.h>
+
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+   printf("%s\n", _POSIX_C_SOURCE);
    return 0;
 }
