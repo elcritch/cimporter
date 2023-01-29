1c87f8e6b860422bd524b2470cf154f8110b5780
Jaremy Creechley <creechley@gmail.com>
2023-01-28 21:46:00 -0700

    add dels

--- tests/example/src/example/sub/subexample.nim
+++ tests/example/src/example/sub/subexample.nim
@@ -1,1 +1,3 @@
 proc subexample*(): cint
+proc subexample2*(): cint
+proc otherexample*(): cint
--- tests/example_includes/example/sub/subexample.h
+++ tests/example_includes/example/sub/subexample.h
@@ -2,3 +2,11 @@
 int subexample();
 
 int delme();
+
+int subexample2();
+
+int delsA();
+int delsB();
+int delsC();
+int delsD();
+int otherexample();
